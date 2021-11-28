f043a989270e222a248222ed81c14f04dac0dc1e
albert <trott.alik@gmail.com>
2021-11-29 00:03:04 +0500

    feat: initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+    res.send("git hash: 1234");
+});
+
+app.listen(port, () => console.log(`Server is running http://localhost:3000`));
