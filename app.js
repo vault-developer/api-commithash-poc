81fc5bce3981c917cd9161f61d6ee76fc0cd8ab8
albert <trott.alik@gmail.com>
2021-11-29 16:14:30 +0500

    ci: update to Heroku deployment config 9

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const app = express();
 
-// if (process.env.NODE_ENV !== 'production') require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
-
-const SHA = process.env.HD_COMMIT_SHA || 'not_found4';
+const SHA = process.env.COMMIT_SHA || 'not_found4';
 
 app.get("/", (req, res) => {
-    res.send(`git hash: ${SHA}, ${JSON.stringify(process.env)}`);
+    res.send(`git hash: ${SHA}`);
 });
 
 app.listen(process.env.PORT || 3000, () => console.log("Server is running..."));
